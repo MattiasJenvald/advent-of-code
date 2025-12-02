3b7bc974492da8dc1f21f5fdeeb9a71cc6243f1f
Mattias Jenvald <mattias.jenvald@gmail.com>
2025-12-02 23:29:21 +0100

    2025 setup

---
+++ 2025/2025/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    private static readonly Dictionary<int, Action<string>> Solvers = new();
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter day number: ");
+        int day = int.Parse(Console.ReadLine()!);
+
+        var inputPath = Path.Combine("inputs", $"day{day:00}.txt}");
+        if (File.Exists(inputPath) == false)
+        {
+            Console.WriteLine($"Input file not found: {inputPath}");
+            return;
+        }
+
+        var input = File.ReadAllText(inputPath);
+
+
+    }
+}
